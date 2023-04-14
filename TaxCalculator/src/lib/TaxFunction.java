f7a79fa9f9ce5391c25cfc55466cbdc3564ffb43
glenrioariesto <glenrioariesto@gmail.com>
2023-04-14 21:07:34 +0700

    memperpendek param yang ada pada function calculateTax

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -15,11 +15,11 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+	public static int calculateTax(Salary salaryData , boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
 		
-		if (numberOfMonthWorking > 12) {
+		if (salaryData.getMonthWorkingInYear() > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 		
@@ -28,9 +28,9 @@ public class TaxFunction {
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+			tax = (int) Math.round(0.05 * (((salaryData.getMonthlySalary() + salaryData.getOtherMonthlyIncome()) * salaryData.getMonthWorkingInYear()) - salaryData.getAnnualDeductible() - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(0.05 * (((salaryData.getMonthlySalary() + salaryData.getOtherMonthlyIncome()) * salaryData.getMonthWorkingInYear()) - salaryData.getAnnualDeductible() - 54000000));
 		}
 		
 		if (tax < 0) {
